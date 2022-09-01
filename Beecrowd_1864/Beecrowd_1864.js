0ebbfd6d1fef8d7271c7c63c4fbcb2256393ac2d
João Bergallo <jhbergallo@gmail.com>
2022-09-01 20:31:27 -0300

    Problems 1864 and 1078

---
+++ Beecrowd_1864/Beecrowd_1864.js
@@ -0,0 +1,5 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+let frase = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+let N = parseInt(lines.shift());
+console.log(`${frase.substring(0, N)}`);
